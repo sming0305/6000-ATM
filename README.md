6bb1080f8ee8694875f9d0d55782a122cf3759f5
sming0305 <sming0305@gmail.com>
2023-04-26 17:51:36 +0800

    新增README.md 及 增加整理JS檔案註解說明

---
+++ README.md
@@ -0,0 +1,22 @@
+# 全民共享普發現金-ATM地圖
+<img alt="全民共享普發現金-ATM地圖" src="https://imgur.com/892be075-8607-46ec-9c69-c6bb1e12ef75">
+
+全民共享普發現金-ATM地圖
+
+參訪網址請點 : [地圖](https://sming0305.github.io/6000-ATM-Map/)
+
+### 相關內容介紹:
+- 每次拖曳地圖後，會顯示鄰近約3*3公里範圍內可提領"普發現金"之ATM機台。
+- 若拖曳地圖至某地點，該區無顯示相關ATM機台地標，則代表該區域無相關ATM機台可使用。
+- 其餘注意事項參考地圖右上角"使用說明按鈕"
+- 使用JSON-Server搭配Render存放ATM地點資料，模擬串接後端API。
+- ATM資料部屬於 Render，若首次開啟讀取時間超過25秒，則請稍待1 - 2分鐘待伺服器啟動。
+
+### 使用技術.套件.資源
+- Google Map API & 叢集標記
+- JSON-Server
+- Render
+- Axios
+- bootstrap
+- spin.js
+- 數位發展部-政府資料開放平臺「全民共享普發現金ATM裝設地點明細」
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// 存放網路請求ATM地點
 let data = [];
 
+// Loading 圖標設定 (spin.js)
 let opts = {
     lines: 13,
     length: 30,
@@ -25,29 +27,33 @@ let opts = {
   target = document.getElementById("spinner"),
   spinner = new Spinner(opts).spin(target);
 
-
+// Loading 背景
 loadingBackground = document.querySelector(".loadingBackground");
 
-const R = 6371; // 地球半徑
-const distance = 2; // 距離
+const R = 6371; // 地球半徑-公里(實際約為6,357km到6,378km)
+const distance = 2; // 每次搜尋並顯示ATM的範圍邊長
 
+//地圖初始中心點之經緯度
 let lat = 25.032;
 let lng = 121.523;
 
+// 距離單位與角度單位換算,計算出偏移度數 (緯度和經度的單位是角度，距離的單位是公里)
 const deltaLat = (distance / R) * (180 / Math.PI);
 const deltaLng =
   ((distance / R) * (180 / Math.PI)) / Math.cos((lat * Math.PI) / 180);
 
+//  計算出南西角座標（swLat, swLng）、東北角座標（neLat, neLng）。
 let swLat = lat - deltaLat;
 let swLng = lng - deltaLng;
 let neLat = lat + deltaLat;
 let neLng = lng + deltaLng;
 
+// 取得ATM地點資料集
 axios
   .get("https://atm-6000.onrender.com/data")
   .then((res) => {
     data = res.data;
-
+    // 取得資料後地圖初始化 , loading結束 , loading背景消除
     initMap();
     spinner.stop();
     loadingBackground.setAttribute("class", "d-none");
@@ -56,7 +62,9 @@ axios
     console.log(err);
   });
 
+// Google Map 初始化函式
 function initMap() {
+  // 整理出符合地圖中心點向外3*3公里的ATM資料集
   let filteredData = data.filter((item) => {
     if (
       item.座標Y軸 >= swLat &&
@@ -64,7 +72,6 @@ function initMap() {
       item.座標X軸 >= swLng &&
       item.座標X軸 <= neLng
     ) {
-      // 這個地標在範圍內
       return {
         lat: item.座標Y軸,
         lng: item.座標X軸,
@@ -72,6 +79,7 @@ function initMap() {
     }
   });
 
+  // 將3*3公里ATM資料集整理成Google Map 叢集markers較易使用的格式
   let locations = filteredData.map((item) => {
     return {
       lat: item.座標Y軸,
@@ -79,16 +87,20 @@ function initMap() {
     };
   });
 
+  // 創建一個Google Map, zoom 等級 10
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat, lng },
   });
+  // 地圖標記資訊
   const infoWindow = new google.maps.InfoWindow({
     content: "",
     disableAutoPan: true,
   });
 
+  // 將ATM地點轉換為markers陣列，並替每個marker添加內容,位置,綁定點擊事件
   const markers = locations.map((position, i) => {
+    // 每一個地點的說明
     const template = `
     <div class="p-1 rounded-1">
     <h2 class="fs-4 fw-bold">ATM名稱 : ${filteredData[i].裝設地點}</h2>
@@ -116,6 +128,7 @@ function initMap() {
     </ul>
     </div>`;
 
+    // 創建marker (位置position ,template內容)
     const marker = new google.maps.Marker({
       position,
       template,
@@ -123,6 +136,7 @@ function initMap() {
 
     // markers can only be keyboard focusable when they have click listeners
     // open info window when marker is clicked
+    // 替每個marker 綁定點擊，點擊後觸發其內容
     marker.addListener("click", () => {
       infoWindow.setContent(template);
       infoWindow.open(map, marker);
@@ -130,9 +144,11 @@ function initMap() {
     return marker;
   });
 
-  // Add a marker clusterer to manage the markers.
+  // Add a marker clusterer to manage the markers.  創建叢集地圖，使用整理好的 markers陣列
   const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
 
+  // 替Google Map 綁定 "拖曳結束"事件
+  // 當"拖曳結束時"觸發以下內容，重新計算並顯示該區域內的ATM地標
   google.maps.event.addListener(map, "dragend", function () {
     // 獲取地圖中心座標
     const center = map.getCenter();
@@ -141,11 +157,13 @@ function initMap() {
     lat = center.lat();
     lng = center.lng();
 
+    // 重新計算出南西角座標（swLat, swLng）、東北角座標（neLat, neLng）。
     swLat = lat - deltaLat;
     swLng = lng - deltaLng;
     neLat = lat + deltaLat;
     neLng = lng + deltaLng;
 
+    // 重新整理出符合地圖中心點向外3*3公里的ATM資料集
     const filteredData = data.filter((item) => {
       return (
         item.座標Y軸 >= swLat &&
@@ -155,7 +173,7 @@ function initMap() {
       );
     });
 
-    // 將新的資料顯示在地圖上
+    // 重新整理出叢集markers較易使用的格式
     const locations = filteredData.map((item) => {
       return {
         lat: item.座標Y軸,
@@ -163,9 +181,12 @@ function initMap() {
       };
     });
 
+    // 清除拖曳前的地標集
     markerCluster.clearMarkers();
 
+    // 添加重新計算過的地標集
     markerCluster.addMarkers(
+      // 此處結果獲得一個新的markers陣列，內含每個marker的地點經緯度及說明內容。
       locations.map((position, i) => {
         const template = `
         <div class="p-1 rounded-1">
@@ -209,4 +230,5 @@ function initMap() {
   });
 }
 
+// 地圖初始化
 window.initMap = initMap;
